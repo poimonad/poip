4c8e601b43dca2f1eacb71e358ae8a7fdd9b0828
poimonad <poimonadT@gmail.com>
2025-02-19 01:31:51 +0000

    feat

--- poip/src/Type.hs
+++ poip/src/Type.hs
@@ -5,7 +5,9 @@ import Prelude hiding (lookup)
 import qualified Data.List as L (union, lookup)
 import qualified Data.Map as M (Map, insert, lookup)
 import Data.Maybe (isJust)
-import Data.Foldable (find)
+import Data.Foldable (find, msum)
+import Data.Either (isRight)
+import Data.List (intersect)
 
 data Kind
   = Star
@@ -30,9 +32,9 @@ data Qual t = [Pred] :=> t deriving (Show, Eq)
 
 data Pred = Constraint Id Type deriving (Show, Eq)
 
-newtype Class = Class ([Id], [Inst]) deriving (Show, Eq)
+type Class = ([Id], [Inst])
 
-newtype Inst = Qual Pred deriving (Show, Eq)
+type Inst = Qual Pred
 
 newtype ClassEnv = ClassEnv { classes :: M.Map Id Class } deriving (Show, Eq)
 
@@ -62,6 +64,10 @@ instance Types Type where
   tv (TApp l r) = tv l `L.union` tv r
   tv _ = []
 
+instance Types Pred where
+  apply s (Constraint i t) = Constraint i $ apply s t
+  tv (Constraint _ t) = tv t
+
 class HasKind t where
   kind :: t -> Maybe Kind
 
@@ -79,7 +85,6 @@ instance HasKind Type where
       Just (Kfun _ k) -> Just k
       _ -> Nothing
   kind (TGen _) = Nothing
-  
 
 mgu :: Type -> Type -> Either String Subst
 mgu (TApp l1 r1) (TApp l2 r2) = do
@@ -98,13 +103,90 @@ mguVar v t
   | kind v /= kind t  = Left "kind do not match" 
   | otherwise         = return $ v +-> t
 
+merge :: Subst -> Subst -> Either String Subst
+merge s1 s2 = if agree then return (s1++s2) else Left "merge"
+   where agree = all (\v -> apply s1 (TVar v) == apply s2 (TVar v))
+                     (fmap fst s1 `intersect` fmap fst s2)
+
+match :: Type -> Type -> Either String Subst
+match (TApp l1 r1) (TApp l2 r2) = do
+  sl <- match l1 l2
+  sr <- match r1 r2
+  sl `merge` sr
+match (TVar v) t | kind v == kind t = return $ v +-> t
+match (TCon c1) (TCon c2) | c1 == c2 = return []
+match _ _ = Left "match: fail"
+
+mguPred :: Pred -> Pred -> Either String Subst
+mguPred (Constraint i1 t1) (Constraint i2 t2)
+  | i1 == i2 = mgu t1 t2
+  | otherwise = Left $ show i1 ++ " and " ++ show i2 ++ " is different class"
+
+super :: Id -> ClassEnv -> Either String [Id]
+super i ce = 
+  case M.lookup i (classes ce) of
+    Just  (is, _) -> Right is
+    Nothing -> Left "super class not found"
+
+insts :: Id -> ClassEnv -> Either String [Inst]
+insts i ce = 
+  case M.lookup i (classes ce) of
+    Just (_, its) -> Right its
+    Nothing -> Left "instance not found"
+
 addClass :: Id -> [Id] -> ClassEnvTransformer
 addClass i is ce
   | defined i ce = Left $ "class " ++ show i ++ " is already defined."
   | (not . all (`defined` ce)) is = 
       case find (not . (`defined` ce)) is of
         Just s -> Left $ "super class " ++ show s ++ " is not defined."
-  | otherwise = return ce { classes = M.insert i (Class (is, [])) $ classes ce }
+  | otherwise = return ce { classes = M.insert i (is, []) $ classes ce }
+
+addInst :: [Pred] -> Pred -> ClassEnvTransformer
+addInst ps p@(Constraint i _) ce = do
+  its <- insts i ce
+  super' <- super i ce
+  let c = (super', (ps :=> p) : its)
+  let qs = [ q | (_ :=> q) <- its]
+  if not (defined i ce) then
+    Left $ "no class for instance " ++ show i 
+  else if any (overlap p) qs then
+    Left $ "class " ++ show p ++ " is overlap"
+  else
+    return (ce { classes = M.insert i c (classes ce) })
+
+overlap :: Pred -> Pred -> Bool
+overlap p q = isRight (mguPred p q)
 
 defined :: Id -> ClassEnv -> Bool
 defined i ce = isJust . M.lookup i $ classes ce
+
+-- あるクラスのインスタンス宣言からスーパークラスをRollup抽出
+bySuper :: Pred -> ClassEnv -> [Pred]
+bySuper p@(Constraint i t) ce = do
+  case super i ce of
+    Right is -> p : concat [ bySuper (Constraint i' t) ce | i' <- is ]
+    Left _ -> [p]
+
+byInst :: Pred -> ClassEnv -> Either String [Pred]
+byInst p@(Constraint i _) ce = do
+   its <- insts i ce
+   concat <$> mapM tryInst its
+  where
+    tryInst :: Inst -> Either String [Pred]
+    tryInst (ps :=> h) = do
+      case mguPred h p of
+        Right u -> Right $ map (apply u) ps
+        Left _ -> Right []
+    -- これ罠
+    -- 型代入がないということは、型が成り立つためにこれ以上証明するべきものがないということ
+
+entail :: ClassEnv -> [Pred] -> Pred -> Bool
+entail ce ps p = any (p `elem`) (map (`bySuper` ce) ps) ||
+  case byInst p ce of
+    Left _ -> False
+    Right qs -> all (entail ce ps) qs
+
+infixr 0 <#>
+(<#>) :: Functor f => f a -> (a -> b) -> f b
+(<#>) = flip (<$>)
--- poip/test/TypeSpec.hs
+++ poip/test/TypeSpec.hs
@@ -2,8 +2,9 @@ module TypeSpec (spec) where
 
 import Test.Hspec (it, describe, Spec, Expectation, shouldBe, shouldSatisfy)
 import Type
-import Data.Either (isLeft)
+import Data.Either (isLeft, fromRight)
 import Control.Monad ((>=>))
+import Type
 
 spec :: Spec
 spec = do
@@ -29,10 +30,29 @@ tests = [
         , ("occur check: unification 'a' and 't a' is occurency case", shouldErr $ mgu (TVar a) (t (TVar a)))
     ])
     , ("Class", [
-      ("Super class defination", shouldErr $ (addClass "Eq" [] >=> addClass "Int" ["Eq", "Show"]) emptyClass)
-      , ("Duplicate class declaration", shouldErr $ (addClass "Eq" [] >=> addClass "Eq" []) emptyClass)
+        ("Super class defination", shouldErr $ (addClass "Eq" [] >=> addClass "Int" ["Eq", "Show"]) emptyClass)
+        , ("Duplicate class declaration", shouldErr $ (addClass "Eq" [] >=> addClass "Eq" []) emptyClass)
+        , ("Fetch Super Class", (super "Int" <$> (addClass "Eq" [] >=> addClass "Int" ["Eq"]) emptyClass) `shouldBe` Right (Right ["Eq"]))
+    ])
+    , ("Instance", [
+        ("No Class For Instance", shouldErr $ addInst [Constraint "Eq" tPrim] (Constraint "Ord" tPrim) emptyClass)
+        , ("Overlap Instance", shouldErr $ (addClass "Eq" [] >=> addClass "Ord" [] >=> addInst [Constraint "Eq" tPrim] (Constraint "Ord" tPrim) >=> addInst [Constraint "Eq" (TVar a)] (Constraint "Ord" (TVar a))) emptyClass)
+    ])
+    , ("Fetch Super Class", [
+        ("", (bySuper <$> pure (Constraint "Ord" tPrim) <*> ((addClass "Eq" [] >=> addClass "Ord" ["Eq"]) emptyClass)) `shouldBe` Right [Constraint "Ord" (TCon (TyCon "prim" Star)),Constraint "Eq" (TCon (TyCon "prim" Star))])
+    ])
+    , ("Fetch Instance", [
+        ("", byInst (Constraint "Eq" (t tPrim)) (unsafeRunClass (addClass "Eq" [] >=> addInst [Constraint "Eq" (TVar a)] (Constraint "Eq" (t $ TVar a)))) `shouldBe` Right [Constraint "Eq" tPrim])
+    ])
+    , ("Class Entailment", [
+        ("", (entail (unsafeRunClass $ tClassEnv) [] (Constraint "Eq" tPrim)) `shouldBe` True)
+        , ("", entail (unsafeRunClass (addClass "Eq" [] >=> addInst [] (Constraint "Eq" tPrim) >=> addInst [Constraint "Eq" (TVar a)] (Constraint "Eq" (t (TVar a))))) [] (Constraint "Eq" (t tPrim)) `shouldBe` True)
     ])
   ]
+
+unsafeRunClass ce = case ce emptyClass of Right ce' -> ce'
+
+tClassEnv = (addClass "Eq" [] >=> addClass "Ord" ["Eq"])
 
 tPrim :: Type
 tPrim = TCon (TyCon "prim" Star)
